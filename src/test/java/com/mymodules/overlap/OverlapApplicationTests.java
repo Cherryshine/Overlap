619704d73c155a8ac39d89fd4266555a7924c3ce
Cherryshine <hi.im.cherryshine@gmail.com>
2025-02-04 18:30:09 +0900

    init commit

---
+++ src/test/java/com/mymodules/overlap/OverlapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymodules.overlap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverlapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
