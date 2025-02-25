b6f988a6ea84fc5ad6667b6bbc454371a8a8076b
Cherryshine <hi.im.cherryshine@gmail.com>
2025-02-25 23:51:20 +0900

    User 추상클래스 구분 업데이트

--- src/main/java/com/mymodules/overlap/entity/User.java
+++ src/main/java/com/mymodules/overlap/entity/User.java
@@ -5,7 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
 @Entity
-@DiscriminatorColumn(name = "user_type")
+@DiscriminatorColumn(name = "user_type") // 구분칼럼임. 상속받는 클래스에서 DiscriminatorValue 로 TEMPORARY, KAKAO 로 구분.
 public abstract class User {
     @Id
     private Long id;
