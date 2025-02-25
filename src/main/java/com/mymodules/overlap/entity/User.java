86433d4f80482f9a615aaae67742cad14860b622
Cherryshine <hi.im.cherryshine@gmail.com>
2025-02-25 23:48:36 +0900

    필수패키지 구성

--- src/main/java/com/mymodules/overlap/entity/User.java
+++ src/main/java/com/mymodules/overlap/entity/User.java
@@ -1,4 +1,14 @@
 package com.mymodules.overlap.entity;
 
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+@DiscriminatorColumn(name = "user_type")
 public abstract class User {
+    @Id
+    private Long id;
+    private String username;
+    private String password;
 }
